9c31a1cf53e3ede8a41ba107947bd953d35d875d
jerzka <kjerzmanowska@gmail.com>
2023-05-03 23:56:47 -0500

    Adding route protection with Firebase user authentication
    - creating a credentials file with a private key generated in the Firebase project
    - installing the firebase-admin package with npm
    - importing the Node.js file system module (fs) to load credentials JSON file
    - initializing  the firebase-admin package and connecting to the Firebase project
    - creating auth-token middleware to load the user automatically
    - adding middleware to prevent a user from requesting if he/she is not logged in
    - adding an array of the IDs of users who already have upvoted the article (an extra property to articles in MongoDB)
     - assuring that postedBy property is equal user e-mail

--- src/server.js
+++ src/server.js
@@ -1,43 +1,85 @@
 import  express  from "express";
+import fs from 'fs';
+import admin from 'firebase-admin';
 import { db, connectToDb } from "./db.js";
+
+const credentials = JSON.parse(
+    fs.readFileSync('./credentials.json')
+);
+
+admin.initializeApp({
+    credential: admin.credential.cert(credentials)
+});
 
 const app = express();
 app.use(express.json());
 
+app.use(async (req, res, next) => {
+    const { authtoken } = req.headers;
+    if(authtoken){
+        try{
+            req.user = await admin.auth().verifyIdToken(authtoken);
+        } catch (e){
+            return res.sendStatus(400);
+        }
+    }
+
+    next();
+});
+
 app.get('/api/articles/:name', async (req, res) =>{
     const { name } = req.params;
+    const { uid } = req.user;
 
     const article = await db.collection('articles').findOne({ name });
 
     if(article){
+        const upvoteIds = article.upvoteIds || [];
+        article.canUpvode  = uid && !upvoteIds.includes(uid);
         res.json(article);
     }
     else{
         res.sendStatus(404);
     }
-})
+});
+
+app.use((req, res, next) => {
+    if(req.user){
+        next();
+    } else{
+        res.sendStatus(401);
+    }
+});
 
 app.put('/api/articles/:name/upvote', async (req, res) => {
     const { name } = req.params;
-
-    await db.collection('articles').updateOne({ name }, {
-        $inc: { upvotes: 1 }
-    });
+    const { uid } = req.user;
 
     const article = await db.collection('articles').findOne({ name });
 
-    if(article){    
-        res.json(article);
+    if(article){
+        const upvoteIds = article.upvoteIds || [];
+        const canUpvode  = uid && !upvoteIds.includes(uid);
+        if(canUpvode){
+            await db.collection('articles').updateOne({ name }, {
+                $inc: { upvotes: 1 },
+                $push: {upvoteIds: uid}
+            });
+        }
+
+        const updatedArticle = await db.collection('articles').findOne({ name });
+        res.json(updatedArticle);
     } else
         res.send('The article dosn\'t exist');
 });
 
 app.post('/api/articles/:name/comments', async (req, res) => {
     const { name } = req.params;
-    const { postedBy, text } = req.body;
+    const { text } = req.body;
+    const { email } = req.user;
 
     await db.collection('articles').updateOne({ name }, {
-        $push: { comments: { postedBy, text }},
+        $push: { comments: { postedBy: email, text }},
     });
 
     const article = await db.collection('articles').findOne({ name });
