89e850eb084d6f3a936e5b3e400eada8de5f2c8d
jerzka <kjerzmanowska@gmail.com>
2023-05-19 14:49:17 -0500

    Releasing a full-stack React application
    - adding an app.yaml file to tell Google cloud what environment project needs to run in
    - adding in env-prod.yaml file database environment variable for production
    - creating start script for an app

--- src/db.js
+++ src/db.js
@@ -6,7 +6,7 @@ async function connectToDb(cb){
     const client = new MongoClient(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@portfoliomongodb.tsqt3w4.mongodb.net/?retryWrites=true&w=majority`);
     await client.connect();
 
-    db = client.db('blog-react-db');
+    db = client.db('portfolio-react-db');
     cb()
 }
 
